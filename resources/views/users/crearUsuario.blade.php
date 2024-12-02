4dca2b9ba9be3b1eae9aac89071bdf1c57c3eca3
ruthIvonne <ruth.26.ivonne@gmail.com>
2024-12-02 20:24:39 -0300

    primer controller y primer view

---
+++ resources/views/users/crearUsuario.blade.php
@@ -0,0 +1,7 @@
+<h1> Crear Usuario </h1>
+<form action="{{ route('storeUsuario') }}" method="post">
+    <label for="descripcion">Descripcion</label>
+    <input type="text" name="descripcion">
+    <br>
+    
+</form>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/crearUsuario',[App\Http\Controllers\UsersController::class,'create'])->name('crearUsuario');
+Route::get('/store',[App\Http\Controllers\UsersController::class,'store'])->name('storeUsuario');
