5f47e074ed78dda3797a85bca5ba8bc5d41a7f6f
ruthIvonne <ruth.26.ivonne@gmail.com>
2024-12-13 16:20:49 -0300

    ..

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,5 +1,3 @@
-<!-- resources/views/dashboard/index.blade.php -->
-
 @extends('layouts.app')
 
 @section('content')
@@ -10,51 +8,39 @@
                 <div class="card-header">{{ __('Listado de Cursos') }}</div>
 
                 <div class="card-body">
-        
-                @auth
-                @if(Auth::user()->rol === 'administrador')
-                <a href="{{ route('cursos.create') }}" class="btn btn-primary">Crear Curso</a>
-                @endif
-                @endauth
-                <table class="table mt-4">
-                    <thead>
-                        <tr>
-                            <th>Título</th>
-                            <th>Institución</th>
-                            <th>Plan de Estudio</th>
-                            <th>Duración</th>
-                            <th>Precio</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+        <a href="{{ route('cursos.create') }}" class="btn btn-primary">Crear Curso</a>
+
+        <table class="table mt-4">
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Institución</th>
+                    <th>Plan de Estudio</th>
+                    <th>Duración</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
                 @foreach ($cursos as $curso)
                     <tr>
-                        <td>{{ $loop->iteration }}</td>
                         <td>{{ $curso->titulo }}</td>
                         <td>{{ $curso->institucion }}</td>
+                        <td>{{ $curso->plan_de_estudio }}</td>
                         <td>{{ $curso->duracion }}</td>
-                        <td>${{ number_format($curso->precio, 2) }}</td>
+                        <td>{{ $curso->precio }}</td>
                         <td>
-                        @auth
-                        @if(Auth::user()->rol === 'administrador')   
                             <a href="{{ route('cursos.edit', $curso->id) }}" class="btn btn-warning">Editar</a>
                             <form action="{{ route('cursos.destroy', $curso->id) }}" method="POST" style="display:inline;">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger">Eliminar</button>
                             </form>
-                        @elseif(Auth::user()->rol === 'alumno')
-                        <a href="{{ route('cursos.index') }}" class="btn btn-warning">Comprar</a>
-                        @elseif(Auth::user()->rol === 'docente')
-                        <a href="{{ route('users.index') }}" class="btn btn-warning">Alumnos inscriptos</a>
-                        @endif
-                        @endauth    
                         </td>
                     </tr>
                 @endforeach
-                    </tbody>
-            </table>
+            </tbody>
+        </table>
         </div>
             </div>
         </div>
